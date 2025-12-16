a5f54a2e01a43a863473f62db81fb2e24fac8318
Calira04 <carloseaslira@gmail.com>
2025-12-16 19:17:44 -0300

    Adicionado ./ex009.py - Tabuada

---
+++ tasks/ex009.py
@@ -0,0 +1,23 @@
+"""
+Arquivo: ex009.py
+Autor: Carlos Lira
+Data: 16/12/2025
+Descrição: Tabuada
+"""
+
+var = int(input("Digite um número para ver a tabuada: "))
+
+print(f"Tabuada do {var}:")
+
+print("--"*10)
+print(f"{var} x 1 = {var * 1}")
+print(f"{var} x 2 = {var * 2}")
+print(f"{var} x 3 = {var * 3}")
+print(f"{var} x 4 = {var * 4}")
+print(f"{var} x 5 = {var * 5}")
+print(f"{var} x 6 = {var * 6}")
+print(f"{var} x 7 = {var * 7}")
+print(f"{var} x 8 = {var * 8}")
+print(f"{var} x 9 = {var * 9}")
+print(f"{var} x 10 = {var * 10}")
+print("--"*10)
