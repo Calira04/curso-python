4f23394b4e3ccd5b8cfe6c109b6d5c1607f6eff6
Calira04 <carloseaslira@gmail.com>
2025-12-15 21:34:26 -0300

    Adicionado ./ex005.py - Antecessor e Sucessor

---
+++ tasks/ex005.py
@@ -0,0 +1,11 @@
+"""
+Arquivo: ex005.py
+Autor: Carlos Lira
+Data: 15/12/2025
+Descrição: Antecessor e Sucessor
+"""
+
+n = int(input("Digite um número inteiro: "))
+antecessor = n - 1
+sucessor = n + 1
+print(f"O antecessor de {n} é {antecessor} e o sucessor é {sucessor}.")
