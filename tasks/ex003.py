695a42b171d2e22b375d31be6d4d87dd73c3f107
Calira04 <carloseaslira@gmail.com>
2025-12-15 21:01:38 -0300

    Adicionado ./ex003.py - Somando dois numeros

---
+++ tasks/ex003.py
@@ -0,0 +1,11 @@
+"""
+Arquivo: ex003.py
+Autor: Carlos Lira
+Data: 15/12/2025
+Descrição: Somando dois numeros
+"""
+
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+soma = n1 + n2
+print(f"A soma de {n1} e {n2} é {soma}.")
