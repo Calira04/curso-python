9c11b2599260eeea349cdd920ace4059bcc7b20c
Calira04 <carloseaslira@gmail.com>
2025-12-16 19:08:00 -0300

    Adicionado ./ex006.py - Dobro, triplo e raiz

---
+++ tasks/ex006.py
@@ -0,0 +1,14 @@
+"""
+Arquivo: ex006.py
+Autor: Carlos Lira
+Data: 16/12/2025
+Descrição: Dobro, Triplo, Raiz Quadrada
+"""
+
+n = float(input("Digite um número: "))
+dobro = n * 2
+triplo = n * 3
+raiz_quadrada = n ** 0.5
+print(f"O dobro de {n} é {dobro}.")
+print(f"O triplo de {n} é {triplo}.")
+print(f"A raiz quadrada de {n} é {raiz_quadrada:.2f}.")
