78110d49e2cb6269969dc3fd78df17ae7f721176
Calira04 <carloseaslira@gmail.com>
2025-12-16 19:11:52 -0300

    Adicionado ./ex007.py - Media Aritimetica

---
+++ tasks/ex007.py
@@ -0,0 +1,13 @@
+"""
+Arquivo: ex007.py
+Autor: Carlos Lira
+Data: 16/12/2025
+Descrição: Media Aritimetica
+"""
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+print("A média aritmética das notas é:", media)
