f67be842b779dbec54e05376a4ab89aff06835a8
Calira04 <carloseaslira@gmail.com>
2025-12-18 15:53:18 -0300

    Adicionado ./ex014.py - Conversor de temperatura

---
+++ tasks/ex014.py
@@ -0,0 +1,13 @@
+"""
+Arquivo: ex014.py
+Autor: Carlos Lira
+Data: 18/12/2025
+Descrição: Conversor de Temperatura
+"""
+
+temp = float(input("Digite a temperatura em Celsius: "))
+
+temp_fahrenheit = (temp * 9/5) + 32
+
+print(f"A temperatura em Fahrenheit é: {temp_fahrenheit:.2f} °F")
+print("Conversão de temperatura concluída com sucesso!")
