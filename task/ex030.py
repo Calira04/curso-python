55696a23f38af03ea8122753e67de78b74cb9520
Calira04 <carloseaslira@gmail.com>
2025-12-20 11:09:11 -0300

    Adicionado ./ex030.py - Par ou Impar

---
+++ task/ex030.py
@@ -0,0 +1,13 @@
+"""
+Arquivo: ex030.py
+Autor: Carlos Lira
+Data: 20/12/2025
+Descrição: Pao ou Impar
+"""
+
+num = int(input("Digite um número: "))
+
+if num % 2 == 0:
+    print(f"O número {num} é Par")
+else:
+    print(f"O número {num} é Ímpar")
