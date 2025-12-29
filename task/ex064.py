71e8bace007d93e0a24b96442dd447f685091585
Calira04 <carloseaslira@gmail.com>
2025-12-29 19:57:36 -0300

    Adiciona ./ex064.py - Tratando varios valores

---
+++ task/ex064.py
@@ -0,0 +1,15 @@
+"""
+Arquivo: ex064.py
+Autor: Carlos Lira
+Data: 29/12/2025
+Descrição: Programa que lê vários números inteiros do usuário e calcula a soma desses números.
+"""
+
+num = 0
+valor = 0
+
+while valor != 999:
+    valor = int(input('Digite um número: '))
+    if valor != 999:
+        num += valor  
+print(num)
